e7878c600e606c2a3cefb445ce13267238480568
mathieumontanino-wq <mathieumontanino@gmail.com>
2026-03-06 22:17:25 +0100

    Création des modèles Mongoose : Catway, Reservation et User

---
+++ models/catway.js
@@ -0,0 +1,24 @@
+/**
+ * Modèle Catway
+ */
+
+const mongoose = require("mongoose");
+
+const catwaySchema = new mongoose.Schema({
+  catwayNumber: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  catwayType: {
+    type: String,
+    enum: ["short", "long"],
+    required: true
+  },
+  catwayState: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("Catway", catwaySchema);
