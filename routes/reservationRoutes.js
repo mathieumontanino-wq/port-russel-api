b772dc06509612931d52d1c195a463f1d6f79788
mathieumontanino-wq <mathieumontanino@gmail.com>
2026-03-07 09:55:42 +0100

    Ajout route PUT /catways/:id/reservations/:idReservation et test fonctionnel

--- routes/reservationRoutes.js
+++ routes/reservationRoutes.js
@@ -15,6 +15,35 @@ router.get("/catways/:id/reservations", async (req, res) => {
 
 });
 
+
+const mongoose = require("mongoose");
+
+// GET une réservation précise
+router.get("/catways/:id/reservations/:idReservation", async (req, res) => {
+
+  try {
+
+    const idReservation = req.params.idReservation;
+
+    // Vérifie si l'id est un ObjectId valide
+    if (!mongoose.Types.ObjectId.isValid(idReservation)) {
+      return res.status(400).json({ message: "ID de réservation invalide" });
+    }
+
+    const reservation = await Reservation.findById(idReservation);
+
+    if (!reservation) {
+      return res.status(404).json({ message: "Réservation non trouvée" });
+    }
+
+    res.json(reservation);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+
+});
+
 // POST créer une réservation pour un catway
 router.post("/catways/:id/reservations", async (req, res) => {
 
@@ -29,6 +58,34 @@ router.post("/catways/:id/reservations", async (req, res) => {
   const savedReservation = await newReservation.save();
 
   res.json(savedReservation);
+
+});
+
+// PUT modifier une réservation
+router.put("/catways/:id/reservations/:idReservation", async (req, res) => {
+
+  try {
+
+    const reservation = await Reservation.findByIdAndUpdate(
+      req.params.idReservation,
+      {
+        clientName: req.body.clientName,
+        boatName: req.body.boatName,
+        startDate: req.body.startDate,
+        endDate: req.body.endDate
+      },
+      { new: true } // renvoie la réservation modifiée
+    );
+
+    if (!reservation) {
+      return res.status(404).json({ message: "Réservation non trouvée" });
+    }
+
+    res.json(reservation);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 
 });
 
