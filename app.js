ebc87f7f05d098babaf4a83d6bbdd30e94c80dce
mathieumontanino-wq <mathieumontanino@gmail.com>
2026-03-08 09:17:38 +0100

    Ajout interface web complète

--- app.js
+++ app.js
@@ -23,15 +23,44 @@ const catwayRoutes = require("./routes/catwayRoutes");
 const reservationRoutes = require("./routes/reservationRoutes");
 const userRoutes = require("./routes/userRoutes");
 const auth = require("./middleware/auth");
+const Catway = require("./models/catway");
+const Reservation = require("./models/reservation");
+const User = require("./models/user");
+
 app.use("/", userRoutes);
 app.use("/catways", auth, catwayRoutes);
 app.use("/", auth, reservationRoutes);
+
 // Route de test
 app.get("/", (req, res) => {
   res.render("index");
 });
 app.get("/dashboard", (req, res) => {
   res.render("dashboard");
+});
+app.get("/dashboard", (req, res) => {
+  res.render("dashboard");
+});
+app.get("/catways-page", async (req, res) => {
+
+  const catways = await Catway.find();
+
+  res.render("catways", { catways });
+
+});
+app.get("/reservations-page", async (req, res) => {
+
+  const reservations = await Reservation.find();
+
+  res.render("reservations", { reservations });
+
+});
+app.get("/users-page", async (req, res) => {
+
+  const users = await User.find();
+
+  res.render("users", { users });
+
 });
 
 module.exports = app;
