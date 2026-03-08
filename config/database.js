eab0eb9b60f99d1ecdd72ea5216fbff316cd65f9
mathieumontanino-wq <mathieumontanino@gmail.com>
2026-03-08 10:30:08 +0100

    Fix MongoDB Atlas connection

--- config/database.js
+++ config/database.js
@@ -1,18 +1,18 @@
-/**
- * Configuration de la connexion à MongoDB
- */
-
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/port-russel");
+    try {
 
-    console.log("Connexion à MongoDB réussie");
-  } catch (error) {
-    console.error("Erreur de connexion MongoDB :", error);
-    process.exit(1);
-  }
+        await mongoose.connect(process.env.MONGO_URI);
+
+        console.log("Connexion à MongoDB réussie");
+
+    } catch (error) {
+
+        console.error("Erreur de connexion MongoDB :", error);
+        process.exit(1);
+
+    }
 };
 
 module.exports = connectDB;
